77c70100b0b17c7086f57c1bb365e9d3097793d6
emredav <65634861+emredav@users.noreply.github.com>
2022-07-10 04:12:10 +0300

    iki notun ortalamasını bulan program

---
+++ odev1.py
@@ -0,0 +1,31 @@
+not1=int(input("not 1 gir:"))
+
+if not1 > 100 or not1 < 0 :
+    print("yanlıs not girdiniz (0-100 arasinda olmalı!)")
+    not1=int(input("not 1 gir:"))
+
+not2=int(input("not 2 gir:"))
+
+if not2 > 100 or not1 < 0 :
+    print("yanlıs not girdiniz (0-100 arasinda olmalı!)")
+    not2=int(input("not 2 gir:"))
+
+ortalama= (not1+not2)/2
+
+if ortalama >= 80: 
+    print("A")
+elif 65 <= ortalama <= 79:
+    print("B")
+elif 55 <= ortalama <= 64:
+    print("C")
+elif 50 <= ortalama <= 54:
+    print("D")
+else:
+    print("F(kaldınız yaz okulunda bol şans)")
+"""
+kaç ders diye sorcaz
+5 diyelim
+sonra girdileri toplam değişkeninde for döngüsünde toplayacağız 
+ondan sonra harf notunu bulcaz
+
+"""
